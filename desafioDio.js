a8d55983bdd94ab6176e9baa859787d83392974d
L3onDay <141137487+L3onDay@users.noreply.github.com>
2023-08-24 11:55:13 -0300

    Add files via upload

---
+++ desafioDio.js
@@ -0,0 +1,24 @@
+let xp = 10001
+let nomeHeroi = " Mestre Jedai "
+let classificacao;
+
+if(xp <= 1000) {
+    classificacao = "Ferro";
+} else if(xp >= 1001 && xp <= 2000) {
+    classificacao = "Bronze";
+} else if(xp >= 2001 && xp <= 5000) {
+    classificacao = "Prata";
+} else if(xp >= 6001 && xp <= 7000) {
+    classificacao = "Ouro";
+} else if(xp >= 7001 && xp <= 8000) {
+    classificacao = "Platina";
+} else if(xp >= 8001 && xp <= 9000) {
+    classificacao = "Ascendente";
+} else if(xp >= 9001 && xp <= 10000) {
+    classificacao = "Imortal";
+} else {
+    classificacao = "Radiante";
+}
+
+console.log("O Herói de nome **"+ nomeHeroi +"** está no nível de "+ classificacao)
+
